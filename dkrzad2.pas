f538a07a2b117b3d9a0428f36e64da41754eb61d
1loYed1 <144238426+1loYed1@users.noreply.github.com>
2024-02-27 12:59:12 +0300

    Add files via upload

---
+++ dkrzad2.pas
@@ -0,0 +1,47 @@
+﻿Program Kr2_zad2;
+var
+  words : array of string;
+  z: array of integer;
+  k, i, max, x: integer;
+  s: string;
+begin
+  write('Введите строку : ');
+  readln(s);
+  setlength(words, (length(s) div 2) + 1);
+  setlength(z, length(words));
+  k := 0;
+  for i := 1 to length(s) do
+  begin
+    if s[i] <> ' ' then
+      words[k] := words[k] + s[i]
+    else
+      k := k + 1;
+  end;
+  for i := 0 to (length(words) - 1) do
+  begin
+    if words[i] <> '' then
+    begin
+      z[i] := 1;
+      for k := i + 1 to (length(words) - 1) do
+      begin
+        if words[i] = words[k] then
+        begin
+          z[i] := z[i] + 1;
+          words[k] := '';
+        end;
+      end;
+    end;
+  end;
+  max := 0;
+  x := 0;
+  for i := 0 to (length(words) - 1) do
+  begin
+    if z[i] > max then
+    begin
+      max := z[i];
+      x := i;
+    end;
+  end;
+
+  write('Часто повторяемое слово: ' ,  words[x]);
+end.
---
+++ drkzad1.pas
@@ -0,0 +1,15 @@
+﻿Program Kr2_zad1;
+var
+  a:array [1..10] of integer;
+  i,min:integer;
+begin
+  for i:=1 to 10 do begin
+    read(a[i]) ;
+  end;
+  min:=abs(a[1]);
+  for i:=1 to 10 do begin
+    if abs(A[i]) < abs(min) then 
+      min:=A[i];
+  end;
+  write('Минимальный по модулю элемент: ', min);
+end.
